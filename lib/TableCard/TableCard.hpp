17054bf779a811f68d419d5109dfa2e8120b56ad
Agilham <13521118@std.stei.itb.ac.id>
2023-03-12 16:41:49 +0700

    feat : update TableCard

--- lib/TableCard/TableCard.hpp
+++ lib/TableCard/TableCard.hpp
@@ -6,32 +6,30 @@
 #include "../Card/Card.hpp"
 #include "../AbstractClasses/InventoryHolder.hpp"
 
-class TableCard {
+class TableCard : public InventoryHolder<Card> {
     private:
-        Card* t_cards;
         int t_count;
-        const int t_max = 5;
     public:
         // constructor, copy constructor, destructor
         TableCard(); // default ctor
-        TableCard(int, Card*); // user-defined ctor
+        TableCard(std::vector<Card>); // user-defined ctor
         TableCard(const TableCard&); // cctor
         ~TableCard(); // dtor
 
         // setter, getter
         void setTCards(int, Card); // mengubah 1 kartu sesuai indeks pada TableCard
-        void setTCards(Card*); // mengubah semua kartu TableCard
+        void setTCards(std::vector<Card>); // mengubah semua kartu TableCard
         void setTCount(int); // mengubah jumlah kartu TableCard
         Card getTCards(int); // mendapat 1 kartu sesuai indeks pada TableCard
-        Card* getTCards(); // mendapat semua kartu TableCard
+        std::vector<Card> getTCards(); // mendapat semua kartu TableCard
         int getTCount(); // mendapat jumlah kartu TableCard
 
         // other
         void addTCards(Card); // menambahkan 1 kartu pada TableCard
-        void addTCards(Card*); // menambah semua kartu TableCard
+        void addTCards(std::vector<Card>); // menambah semua kartu TableCard
         void removeTCards(); // menghapus 1 kartu pada TableCard
         void clearTCards(); // menghapus semua kartu TableCard
-        void showTCards(); // menampilkan semua kartu TableCard
+        void print(); // menampilkan semua kartu TableCard
 };
 
 #endif
