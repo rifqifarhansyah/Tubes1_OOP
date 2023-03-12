c6c372a63b76ef7eb5049b2f0d116ab744f366c0
Agilham <13521118@std.stei.itb.ac.id>
2023-03-12 15:40:31 +0700

    feat : fix TableCard

--- lib/TableCard/TableCard.hpp
+++ lib/TableCard/TableCard.hpp
@@ -3,8 +3,8 @@
 #ifndef TABLECARD_HPP
 #define TABLECARD_HPP
 
-#include "../ValuedObject/Card.hpp"
-#include "../InventoryHolder/InventoryHolder.hpp"
+#include "../Card/Card.hpp"
+#include "../AbstractClasses/InventoryHolder.hpp"
 
 class TableCard {
     private:
