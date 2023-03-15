110e688335c6b927c5cd91fcf2ed63d6ec855967
Agilham <13521118@std.stei.itb.ac.id>
2023-03-15 14:11:28 +0700

    fix: ability

--- lib/Ability/Quarter.cpp
+++ lib/Ability/Quarter.cpp
@@ -8,7 +8,7 @@ Quarter::Quarter() : Ability("Quarter", 5){}
 void Quarter::action(Player& p,Game& g) const
 {
     if (g.getPoint() != 1) {
-        cout << p.getNamePlayer() << " melakukan QUARTER! Poin hadiah naik dari " << g.getPoint() << " menjadi ";
+        cout << p.getNamePlayer() << " melakukan QUARTER! Poin hadiah turun dari " << g.getPoint() << " menjadi ";
         if (g.getPoint() == 2) {
             g.setPoint(g.getPoint()/2);
         } else {
@@ -18,4 +18,5 @@ void Quarter::action(Player& p,Game& g) const
     } else {
         cout << p.getNamePlayer() << " melakukan QUARTER! Sayangnya poin hadiah sudah bernilai 1. Poin hadiah tidak berubah.. Giliran dilanjut!" << endl;
     }
+    p.removeAbility();
 }
